e75c8a3e5dcce5422200588881019182500a9a01
MarcelloMeneses <mmteixeira89@gmail.com>
2025-09-03 07:34:51 -0300

    Adicionado cron

--- src/main/java/com/meneses/bffagendadortarefas/infrasctructure/client/EmailClient.java
+++ src/main/java/com/meneses/bffagendadortarefas/infrasctructure/client/EmailClient.java
@@ -3,9 +3,11 @@ package com.meneses.bffagendadortarefas.infrasctructure.client;
 
 import com.meneses.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
+    @PostMapping
     void enviarEmail(@RequestBody TarefasDTOResponse dto);
 }
